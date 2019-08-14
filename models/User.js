1933edc9c0488b72a1a314cd28bd763fcb5326df
DevMai90 <dmai90@yahoo.com>
2019-08-13 17:02:46 -0700

    Create User model

---
+++ models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+// Everything is derived from a schema which creates the document's data structure (shape of the document)
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  handle: {
+    type: String,
+    unqiue: true
+  },
+  avatar: {
+    type: String
+  },
+  about: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// Export model as variable called User
+// model name is user and new instances will be put into a users collection on MongoDB
+module.exports = User = mongoose.model('user', UserSchema);
