2e5e6f12531e3f3d1e31ab55e3729bb83bdb6c33
DevMai90 <dmai90@yahoo.com>
2019-08-23 12:53:38 -0700

    Create utility function to validate email address at login. User can login with either their email or username. Mongoose does querying based on which type

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,6 +2,8 @@ const express = require('express');
 // Breaks up our routes into separate files. Routes can then be loaded into server.js
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
+const validateEmailFormat = require('../../utils/validateEmailFormat');
 
 const User = require('../../models/User');
 
@@ -21,5 +23,45 @@ router.get('/', auth, async (req, res) => {
     res.status(500).send('Server error');
   }
 });
+
+// @route   POST /api/auth
+// @desc    Authenticate and retrieve token
+// @access  Public
+// Login via email or user handle
+router.post(
+  '/',
+  [
+    check('login', 'Please enter a registered email address or username')
+      .not()
+      .isEmpty(),
+    check('password', 'Please enter a valid password')
+      .not()
+      .isEmpty()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(422).json({ errors: errors.array() });
+    }
+
+    const { login, password } = req.body;
+
+    try {
+      let user;
+      if (validateEmailFormat(login)) {
+        user = await User.findOne({ email: login });
+      } else {
+        user = await User.findOne({ handle: login });
+      }
+
+      console.log(login);
+      console.log(validateEmailFormat(login));
+      res.send(user);
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
 
 module.exports = router;
---
+++ utils/validateEmailFormat.js
@@ -0,0 +1,4 @@
+module.exports = email => {
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) return true;
+  else return false;
+};
