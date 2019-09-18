3debc5b9ad6d2bd7b17b211fd398432a9647faa2
DevMai90 <dmai90@yahoo.com>
2019-09-18 10:46:38 -0700

    Fix headers bug after request has been sent to client

--- middleware/auth.js
+++ middleware/auth.js
@@ -15,7 +15,7 @@ module.exports = function(req, res, next) {
   const token = req.header('x-auth-token');
 
   // Check if there is no token
-  if (!token) res.status(401).json({ msg: 'No token, not authorized' });
+  if (!token) return res.status(401).json({ msg: 'No token, not authorized' });
 
   // Verify token
   try {
