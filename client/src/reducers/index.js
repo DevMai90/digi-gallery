1b78247e9594a8b3196dfee6940c8c1c0907b9f2
DevMai90 <dmai90@yahoo.com>
2019-09-22 23:57:23 -0700

    Add post reducer to root reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -5,10 +5,12 @@ import { combineReducers } from 'redux';
 // Reducers
 import alert from './alert';
 import auth from './auth';
+import post from './post';
 
 export default combineReducers({
   alert,
-  auth
+  auth,
+  post
 });
 
 /*
